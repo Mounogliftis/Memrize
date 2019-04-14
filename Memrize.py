6bf99dab14b5f0818e1192480df6c6f2d8bb25e7
Daniel Abela <DanielAbela96@gmail.com>
2019-04-14 16:46:18 +0100

    Changed variable and function names

--- Memrize.py
+++ Memrize.py
@@ -3,19 +3,19 @@ poem_lines = []
 user_words = []
 
 
-def tailorUserLine(userLine, poemLine):
-    if len(userLine) < len(poemLine):
-        missingLength = len(poemLine) - len(userLine)
-        for i in range(0, missingLength):
-            userLine.append("NULL")
-        return userLine
-    elif len(userLine) > len(poemLine):
-        excessLength = len(userLine) - len(poemLine)
+def tailor_user_line(userline, poemline):
+    if len(userline) < len(poemline):
+        missing_length = len(poemline) - len(userline)
+        for i in range(0, missing_length):
+            userline.append("NULL")
+        return userline
+    elif len(userline) > len(poemline):
+        excessLength = len(userline) - len(poemline)
         for i in range(0, excessLength):
-            del userLine[-1]
-        return userLine
+            del userline[-1]
+        return userline
     else:
-        return userLine
+        return userline
 
 
 file = open(path_name + str(18) + '.txt', 'r')
@@ -32,14 +32,14 @@ for i in range(1, 15):
         word = word.strip("\n")
         temp_words.append(word)
     poem_lines[i] = temp_words
-    #print(poem_words[i])
+    # print(poem_words[i])
 
 del poem_lines[0]
 for poem_line in poem_lines:
     line = input('Please enter the next line: ')
     user_words = line.split(" ")
 
-    user_words = tailorUserLine(user_words, poem_line)
+    user_words = tailor_user_line(user_words, poem_line)
 
     for i in range(len(user_words)):
         if user_words[i] == poem_line[i]:
